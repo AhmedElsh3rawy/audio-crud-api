89cfa7039019226396b4f668f8f4dc757b816285
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-26 04:59:57 +0300

    feat: add middlewares to parse request body

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { errorHandler, notFound } from "./middleware/errorHandler";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, World!");
 });
