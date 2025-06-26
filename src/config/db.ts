216688a2cfb1a437dce82a637e501805bd5badda
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-26 16:18:02 +0300

    feat: mongoose config for mongodb database

---
+++ src/config/db.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const mongodbUrl = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`;
+
+mongoose
+	.connect(mongodbUrl)
+	.then(() => console.log("MongoDB connected successfully"))
+	.catch((err) => console.error("MongoDB connection error:", err));
