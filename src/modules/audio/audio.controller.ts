5fb0935191c9a067ad79658d43f154d1b72f3f0e
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-26 21:08:32 +0300

    feat: create new audio

--- src/modules/audio/audio.controller.ts
+++ src/modules/audio/audio.controller.ts
@@ -1,7 +1,9 @@
 import type { Request, Response, NextFunction } from "express";
+import type { CreateAudioBody } from "./audio.type";
 import { asyncWrapper } from "../../utils/asyncWrapper";
+import { getDuration } from "../../utils/duration";
+import Audio from "./audio.model";
 
-// TODO: add DB and some fields
 export const createAudio = asyncWrapper(
 	async (req: Request, res: Response, next: NextFunction) => {
 		const files = req.files as {
@@ -12,7 +14,15 @@ export const createAudio = asyncWrapper(
 		if (!files.audio || !files.image) {
 			return res.status(400).json({ error: "Audio and image are required" });
 		}
-		console.log(files);
-		res.status(200).json({ message: "success", files });
+		const duration = await getDuration(files.audio[0].path);
+		const data: CreateAudioBody = req.body as CreateAudioBody;
+		const newAudio = new Audio({
+			...data,
+			duration,
+		});
+		newAudio.fileUrl = files.audio[0].path;
+		newAudio.coverImage = files.image[0].path;
+		await newAudio.save();
+		res.status(200).json({ message: "success", data: newAudio });
 	},
 );
