f9ee88f4debf39065531070a17f06024416543d2
Ahmed <ahmedelsharawy117@gmail.com>
2025-06-27 05:04:14 +0300

    chore: structre mongo url

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const mongodbUrl = `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`;
+const mongodbUrl =
+	`mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}` +
+	`@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`;
 
 mongoose
 	.connect(mongodbUrl)
